98f89187e5a7df5842690cb32d046554c665ccdc
Kshitij Sahu <kshitij.sahu_cs19@gla.ac.in>
2021-04-24 15:19:31 +0530

    method to findBudgetSmartPhone

--- src/Definition/OnlineShop.java
+++ src/Definition/OnlineShop.java
@@ -1,4 +1,20 @@
 package Definition;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class OnlineShop {
+
+    public List<SmartPhone> getListOfSmartPhones(Set<SmartPhone> smartPhones, byte requiredScreenSize) {
+        List<SmartPhone> smartPhoneList = new ArrayList<>();
+        for ( SmartPhone smartPhone : smartPhones){
+            if(smartPhone.getScreenSize() >= requiredScreenSize){
+                smartPhoneList.add(smartPhone);
+            }
+            smartPhone.setPrice(smartPhone.getPrice() - smartPhone.getPrice()*0.1);
+        }
+        return smartPhoneList;
+    }
+
 }

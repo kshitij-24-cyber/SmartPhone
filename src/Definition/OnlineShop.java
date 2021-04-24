d13558d83d2c6d47230560c2a6a2e39354d772ed
Kshitij Sahu <kshitij.sahu_cs19@gla.ac.in>
2021-04-24 15:30:38 +0530

    method to SearchPhone

--- src/Definition/OnlineShop.java
+++ src/Definition/OnlineShop.java
@@ -8,13 +8,31 @@ public class OnlineShop {
 
     public List<SmartPhone> getListOfSmartPhones(Set<SmartPhone> smartPhones, byte requiredScreenSize) {
         List<SmartPhone> smartPhoneList = new ArrayList<>();
-        for ( SmartPhone smartPhone : smartPhones){
-            if(smartPhone.getScreenSize() >= requiredScreenSize){
+        for (SmartPhone smartPhone : smartPhones) {
+            if (smartPhone.getScreenSize() >= requiredScreenSize) {
                 smartPhoneList.add(smartPhone);
             }
-            smartPhone.setPrice(smartPhone.getPrice() - smartPhone.getPrice()*0.1);
+            smartPhone.setPrice(smartPhone.getPrice() - smartPhone.getPrice() * 0.1);
         }
         return smartPhoneList;
     }
+
+    public List<SmartPhone> findBudgetSmartPhone(Set<SmartPhone> smartPhones) {
+        List<SmartPhone> list1 = new ArrayList<>();
+        for (SmartPhone smartPhone : smartPhones) {
+            if (smartPhone.getPrice() >= 10000) {
+                list1.add(smartPhone);
+            }
+        }
+        if (list1.isEmpty()) {
+            list1.add(new SmartPhone(945116296, "samsaung", 10000.0, (byte) 5));
+        }
+        return list1;
+
+
+    }
+
+
+
 
 }
